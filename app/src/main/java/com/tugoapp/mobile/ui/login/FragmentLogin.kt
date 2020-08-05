821c02593c5bc654cacb2a488dd54bf82784e0f2
purvi <purvi@pardypanda.com>
2020-08-05 16:49:49 +0530

    Added validation for signin

--- app/src/main/java/com/tugoapp/mobile/ui/login/FragmentLogin.kt
+++ app/src/main/java/com/tugoapp/mobile/ui/login/FragmentLogin.kt
@@ -2,6 +2,8 @@ package com.tugoapp.mobile.ui.login
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.Navigation
@@ -13,6 +15,7 @@ import com.tugoapp.mobile.ui.base.BaseFragment
 import com.tugoapp.mobile.ui.base.ViewModelProviderFactory
 import com.tugoapp.mobile.utils.CommonUtils
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_signup.*
 import javax.inject.Inject
 
 class FragmentLogin : BaseFragment<LoginViewModel?>() {
@@ -45,6 +48,36 @@ class FragmentLogin : BaseFragment<LoginViewModel?>() {
         mContext = context
         auth = FirebaseAuth.getInstance()
         initControls()
+        initTextChange()
+    }
+
+    private fun initTextChange() {
+        edtEmail.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var data = s.toString()
+                if(!data.isNullOrBlank() && com.tugoapp.mobile.utils.CommonUtils.isEmailValid(data)) {
+                    imgSigninEmailRight.visibility = View.VISIBLE
+                } else {
+                    imgSigninEmailRight.visibility = View.GONE
+                }
+            }
+        })
+
+        edtPswd.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var data = s.toString()
+                if(!data.isNullOrBlank() && data.length >=6 ) {
+                    imgSigninPswdRight.visibility = View.VISIBLE
+                } else {
+                    imgSigninPswdRight.visibility = View.GONE
+                }
+            }
+        })
+
     }
 
     private fun initControls() {
